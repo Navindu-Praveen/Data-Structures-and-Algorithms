8c2940604f54bbe34ba41faf119fece03b063dc4
Navindu-Praveen <talktopra@gmail.com>
2023-11-13 13:07:00 +0530

    node class update
    attributes and methods of node

--- DataStr/src/Node.java
+++ DataStr/src/Node.java
@@ -1,4 +1,18 @@
 public class Node {
+    public int iData; 
+    public double dData; //other data
+    public Node leftChild; //this nodes left child
+    public Node rightChild; //this nodes right child
+
+    public void displyNode(){
+        System.out.println("{");
+        System.out.println(iData);
+        System.out.println(",");
+        System.out.println(dData);
+        System.out.println("}");
+    }
+
+
 
     
 }
